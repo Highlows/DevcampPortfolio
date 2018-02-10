03156b5680d8cc2895d3610350a25d02fd257e11
Adrian Braden <adrianbraden@Adrians-MacBook-Pro.local>
2018-02-09 19:39:56 -0800

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,6 +44,12 @@ end
     thumb_image: "http://via.placeholder.com/350x200"
   )
 end
-
-
 puts "9 portfolio items created."
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+    )
+end
+puts "3 technologies created."
+
